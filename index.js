97769988a347a10d5506dc92f91a6427bd8b5c88
Salman_JS <salman4javascript@gmail.com>
2022-10-25 23:40:24 +0530

    Second commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const routes = require('../routes/Routes.js');
+const routes = require('./routes/Routes.js');
 
 const database = "mongodb+srv://salman:salman@cluster0.gzlgnuh.mongodb.net/?retryWrites=true&w=majority"
 
@@ -25,10 +25,8 @@ mongoose.connection.on("error", () => {
   console.log("Some internal error occured in the database!", err);
 })
 
-app.use("/.netlify/functions/server", routes);
+app.use("/", routes);
 
-// app.listen(3002, () => {
-//   console.log("Server started!");
-// })
-
-module.exports.handler = serverless(app);
+app.listen(3002, () => {
+  console.log("Server started!");
+})
